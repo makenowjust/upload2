375e328533de66559acb4c1482c05f0691174dbe
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-10-13 13:12:38 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "files#index"
+  resources :files, only: %i(show create destroy), as: :file_infos
+  resources :download, only: %i(show)
 end
