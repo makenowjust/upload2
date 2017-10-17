7966ba7ffb281ad354dd6a09980b3eac13134ee3
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-10-18 02:33:06 +0900

    Style with materialize-css

---
+++ config/initializers/action_view_field_error_proc.rb
@@ -0,0 +1,11 @@
+Rails.application.config.action_view.field_error_proc = ->(html_tag, instance) do
+  if instance.is_a?(ActionView::Helpers::Tags::Label)
+    html_tag
+  else
+    Nokogiri::HTML
+      .fragment(html_tag)
+      .search('input', 'textarea', 'select')
+      .add_class('invalid')
+      .to_html.html_safe
+  end
+end
