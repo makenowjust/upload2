1ee00801bc696facd7342d83a3b0b5a77bb29ed6
TSUYUSATO Kitsune <make.just.on@gmail.com>
2017-10-18 02:33:17 +0900

    Add validations for FileInfo

--- app/models/file_info.rb
+++ app/models/file_info.rb
@@ -4,6 +4,8 @@ class FileInfo < ApplicationRecord
   has_secure_password
   has_one :file_content, dependent: :destroy
 
+  validate :validate_file
+
   scope :public_file_infos, -> {
     where(
       "private = :private AND (expiration IS NULL OR expiration >= :expiration)",
@@ -27,5 +29,17 @@ class FileInfo < ApplicationRecord
   def outdated?
     return false unless expiration
     expiration < Time.current
+  end
+
+  private def validate_file
+    if self.name.blank? || self.content_size.blank?
+      errors.add(:file, "can't be empty")
+      return
+    end
+
+    if self.content_size >= 500.kilobytes
+      errors.add(:file, "is too big")
+      return
+    end
   end
 end
